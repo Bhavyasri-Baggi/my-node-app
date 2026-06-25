901fa44ddf9dffa2537b30f34b223c1f851bbb8c
Bhavya Sri Baggi <111896968+Bhavyasri-Baggi@users.noreply.github.com>
2026-06-25 11:17:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('Hello from EC2 🚀');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(8080, () => {
+  console.log('Server running on port 8080');
 });
